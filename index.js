ba6b1203aba9659e17215a24807b546f5ee5d300
davmanukyan94 <102730585+davmanukyan94@users.noreply.github.com>
2024-04-02 15:26:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require("body-parser")
-const dns = require("dns")
-
-
+const bodyParser = require("body-parser");
+const dns = require("dns");
 
 const port = process.env.PORT || 3000;
+let arr = [];
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-app.use(bodyParser.urlencoded({extended:true}))
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/shorturl', function(req, res) {
-  const {url} = req.body
-  const splitUrl = url.split("https://")
-  if(splitUrl[1]==null){
-    res.json({ error: 'invalid url' })
-      return
-  }
-  dns.lookup(splitUrl[1],(err)=>{
-    if(err){
-      console.log(err)
-      res.json({ error: 'invalid url' })
-      return 
+app.post("/api/shorturl", function (req, res) {
+  const { url } = req.body;
+  dns.lookup(url, {}, (err) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+      return;
     }
-    const index = arr.findIndex(item=>item===url)
-    if(index>-1){
-      res.json({original_url:url,short_url:index})
-      return
+    const index = arr.findIndex((item) => item === url);
+    if (index > -1) {
+      res.json({ original_url: url, short_url: index });
+      return;
     }
-    arr.push(url)
-    res.json({original_url:url,short_url:arr.length-1})
-  })
+    arr.push(url);
+    res.json({ original_url: url, short_url: arr.length - 1 });
+  });
 });
 
-app.get('/api/shorturl/:id',(req,res)=>{
-const {id} = req.params
-const url = arr[+id];
-res.redirect(url);
-})
+app.get("/api/shorturl/:id", (req, res) => {
+  const { id } = req.params;
+  const url = arr[+id];
+  res.redirect(`https://${url}`);
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
